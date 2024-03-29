19b63ce73493206841f483d685849a863c6c8da8
enos <enos.salar.10@gmail.com>
2024-03-29 15:34:19 +0300

    Final Version

--- src/FileSizeCalculatorThread.java
+++ src/FileSizeCalculatorThread.java
@@ -17,7 +17,28 @@ public class FileSizeCalculatorThread extends Thread {
     @Override
     public void run() {
         for (File file : files) {
-            fileSizeUsage.addSize(file.length());
+            fileSizeUsage.addSize(calculateFolderSize(file));
         }
+    }
+
+    // Method source: ChatGPT
+    public static long calculateFolderSize(File folder) {
+        long size = 0;
+
+        if (folder.isDirectory()) {
+            File[] files = folder.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isDirectory()) {
+                        size += calculateFolderSize(file); // Recursive call for subfolders
+                    } else {
+                        size += file.length(); // Add file size
+                    }
+                }
+            }
+        } else {
+            size += folder.length(); // Add size of the file if it's not a directory
+        }
+        return size;
     }
 }
