7a043f6b9d8ee23c26f263e5b5e0baca0a511e1c
enos <enos.salar.10@gmail.com>
2024-03-29 15:15:48 +0300

    Final Version

--- src/CalculationResult.java
+++ src/CalculationResult.java
@@ -1,19 +1,19 @@
 public class CalculationResult {
-    private final long timeTaken;
+    private final long timeTakenInNano;
     private final int threadsUsed;
     private final boolean isThreadPoolUsed;
     private final FileSizeUsage fileSizeUsage;
 
     @Override
     public String toString() {
-        return "Total time taken: " + ((double) timeTaken / 1000000.0) + " milliseconds\n" +
+        return "Total time taken: " + ((double) timeTakenInNano / 1000000.0) + " milliseconds\n" +
                 "Directory size: " + fileSizeUsage.getSize() + " bytes\n" +
                 "Threads used: " + threadsUsed + "\n" +
                 "Is Thread pool used: " + isThreadPoolUsed;
     }
 
-    public CalculationResult(long timeTaken, int threadsUsed, boolean isThreadPoolUsed, FileSizeUsage fileSizeUsage) {
-        this.timeTaken = timeTaken;
+    public CalculationResult(long timeTakenInNano, int threadsUsed, boolean isThreadPoolUsed, FileSizeUsage fileSizeUsage) {
+        this.timeTakenInNano = timeTakenInNano;
         this.threadsUsed = threadsUsed;
         this.isThreadPoolUsed = isThreadPoolUsed;
         this.fileSizeUsage = fileSizeUsage;
